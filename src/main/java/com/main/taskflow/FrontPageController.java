c0943a9a2dd8f36410474cd868ab950dc64279f3
Kenji V. Salada <ksalada82524@liceo.edu.ph>
2025-11-16 11:29:42 +0800

    Rename HelloController to FrontPageController

---
+++ src/main/java/com/main/taskflow/FrontPageController.java
@@ -0,0 +1,27 @@
+    package com.main.taskflow;
+
+    import javafx.fxml.FXML;
+    import javafx.fxml.FXMLLoader;
+    import javafx.scene.Parent;
+    import javafx.scene.Scene;
+    import javafx.scene.control.Button;
+    import javafx.stage.Stage;
+
+    import java.util.Objects;
+
+    public class FrontPageController {
+
+        @FXML
+        private Button startButton;
+
+        @FXML
+        private void onStart() {
+            try {
+                Parent dashboard = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("dashboard.fxml")));
+                Stage stage = (Stage) startButton.getScene().getWindow();
+                stage.setScene(new Scene(dashboard, 900, 500));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
