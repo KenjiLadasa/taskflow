a6404cd8280be2ed62ab0156fab36cacf9d3e9ca
Kenji V. Salada <ksalada82524@liceo.edu.ph>
2025-11-16 11:23:51 +0800

    Refactor FrontPageController to HelloController

--- src/main/java/com/main/taskflow/FrontPageController.java
+++ src/main/java/com/main/taskflow/FrontPageController.java
@@ -1,27 +1,14 @@
 package com.main.taskflow;
 
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.stage.Stage;
+import javafx.scene.control.Label;
 
-import java.util.Objects;
-
-public class FrontPageController {
+public class HelloController {
+    @FXML
+    private Label welcomeText;
 
     @FXML
-    private Button startButton;
-
-    @FXML
-    private void onStart() {
-        try {
-            Parent dashboard = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("dashboard.fxml")));
-            Stage stage = (Stage) startButton.getScene().getWindow();
-            stage.setScene(new Scene(dashboard, 900, 500));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
     }
 }
